94ea530a457d7329c99fbf5ef65ae9c7313ce541
Vighnesh Kanakala <63389698+vighnesh777@users.noreply.github.com>
2021-07-02 16:36:09 +0530

    Create booleanMatrix.py

---
+++ booleanMatrix.py
@@ -0,0 +1,31 @@
+def booleanMatrix(matrix):
+    # code here 
+    d1={}
+    l1=[]
+    l2=[]
+    d2={}
+    m=matrix
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if m[i][j]==1:
+                d1[i]=i
+                d2[j]=j
+    for i in d1:
+        l1.append(d1[i])
+    for j in d2:
+        l2.append(d2[j])
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if i in l1 or j in l2:
+                matrix[i][j]=1
+#driver code
+r,c=map(int,input().split())
+m=[]
+for i in range(r):
+  m.append(list(map(int,input().split()))
+booleanMatrix(m)
+for i in range(r):
+  for j in range(c):
+    print(m[i][j])
+  print()
+  
