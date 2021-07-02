47819fda308433e68f78dfb9f0304d04cca6467c
Vighnesh Kanakala <63389698+vighnesh777@users.noreply.github.com>
2021-07-02 16:39:33 +0530

    Update booleanMatrix.py

--- booleanMatrix.py
+++ booleanMatrix.py
@@ -1,3 +1,22 @@
+"""
+Given a boolean matrix of size RxC where each cell contains either 0 or 1,
+modify it such that if a matrix cell matrix[i][j] is 1 then all the cells in its ith row and jth column will become 1.
+
+Example 1:
+
+Input:
+R = 2, C = 2
+matrix[][] = {{1, 0},
+              {0, 0}}
+Output: 
+1 1
+1 0 
+Explanation:
+Only cell that has 1 is at (0,0) so all 
+cells in row 0 are modified to 1 and all 
+cells in column 0 are modified to 1.
+"""
+
 def booleanMatrix(matrix):
     # code here 
     d1={}
